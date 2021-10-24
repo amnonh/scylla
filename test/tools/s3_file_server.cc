d7bad4862b6538e88fcbcb971d2f47921489f3d6
Amnon Heiman <amnon@scylladb.com>
2021-10-24 22:30:16 +0300

    s3_file_server.cc: support range header

--- test/tools/s3_file_server.cc
+++ test/tools/s3_file_server.cc
@@ -37,6 +37,61 @@ public:
     ~file_writer_handler() = default;
 };
 
+std::tuple<int, int> split_range(const sstring& range) {
+    auto eql = range.find("=");
+    auto pos = range.find("-", eql + 1);
+    size_t end;
+    end = pos + 1;
+    return std::make_tuple(std::stoi(range.substr(eql+1, pos)), std::stoi(range.substr(end)));
+}
+
+seastar::future<> copy_n(input_stream<char>& is, uint64_t size, output_stream<char>& os) {
+    bool eof = false;
+    while(size >0  && !eof && !is.eof()) {
+        auto buf = co_await is.read();
+        if (buf.size() > size) {
+            buf.trim(size);
+        }
+        if (!buf.empty()) {
+            size -= buf.size();
+            co_await os.write(buf.get(), buf.size());
+        } else {
+            eof = true;
+        }
+    }
+    co_return;
+}
+
+class directory_handler_s3 : public directory_handler {
+public:
+    directory_handler_s3(const sstring& path) : directory_handler(path) {
+    }
+    virtual future<std::unique_ptr<reply>> read(sstring file_name, std::unique_ptr<request> req,
+            std::unique_ptr<reply> rep) override;
+};
+
+future<std::unique_ptr<reply>> directory_handler_s3::read(
+        sstring file_name, std::unique_ptr<request> req,
+        std::unique_ptr<reply> rep) {
+    sstring extension = get_extension(file_name);
+    rep->write_body(extension, [req = std::move(req), extension, file_name, this] (output_stream<char>&& s) mutable -> seastar::future<> {
+        auto range = req->get_header("Range");
+        auto os = output_stream<char>(get_stream(std::move(req), extension, std::move(s)));
+        auto file = co_await open_file_dma(file_name, open_flags::ro);
+        auto is = make_file_input_stream(file);
+        if (range != "") {
+            auto [from, to] = split_range(range);
+            co_await is.skip(from);
+            co_await copy_n(is, (to-from + 1), os);
+        } else {
+            co_await copy(is, os);
+        }
+        co_await os.close();
+        co_await is.close();
+        co_return ;
+    });
+    return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
+}
 future<std::unique_ptr<reply>> file_writer_handler::handle(const sstring& path,
            std::unique_ptr<request> req, std::unique_ptr<reply> rep) {
     filelog.debug("file_writer_handler {} req {} ", req->_method, get_file_name(path));
@@ -55,7 +110,7 @@ future<> init(const s3_context& ctx, seastar::httpd::http_server_control& http_s
             r.put(GET, "/", new httpd::file_handler(ctx.base_dir + "/index.html",
                     new content_replace("html")));
             r.add(PUT, url("").remainder("path"), new file_writer_handler(ctx.base_dir));
-            r.add(GET, url("").remainder("path"), new httpd::directory_handler(ctx.base_dir));
+            r.add(GET, url("").remainder("path"), new directory_handler_s3(ctx.base_dir));
         });
     });
 }
